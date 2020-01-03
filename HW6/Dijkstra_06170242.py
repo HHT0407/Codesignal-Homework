0a3b6d2a506bfd2c5a4191cd10268540016e2cf7
HHT0407 <35271790+HHT0407@users.noreply.github.com>
2020-01-03 23:22:49 +0800

    Update Dijkstra_06170242.py

--- HW6/Dijkstra_06170242.py
+++ HW6/Dijkstra_06170242.py
@@ -5,20 +5,22 @@ import heapq
 import math
 
   
-  class Graph(object):
-  def __init__(self):
-    self.nodes = set()
-    self.edges = defaultdict(list)
-    self.distances = {}
-
-  def add_node(self, value):
-    self.nodes.add(value)
-
-  def add_edge(self, from_node, to_node, distance):
-    self.edges[from_node].append(to_node)
-    self.edges[to_node].append(from_node)
-    self.distances[(from_node, to_node)] = distance
-
+class Graph(object):
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+        self.graph_matrix = [[0 for column in range(vertices)]
+                             for row in range(vertices)]
+        self.dict = defaultdict(list)
+        
+    def addEdge(self,u,v,w): 
+        self.dict[w].append([u,v])
+    
+    def Dijkstra(self, s):
+        s = str(s)
+        graph_dict = self.get_graph_dict()
+        if not graph_dict:
+            return
 
 def dijsktra(graph, initial):
   visited = {initial: 0}
